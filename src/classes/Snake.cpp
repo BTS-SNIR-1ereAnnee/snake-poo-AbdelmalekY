9276d4462b7ac39eae027123463f8911f1731891
eleve <abdelmalek.younes@lyceeppr.fr>
2020-03-05 12:45:05 +0100

    Changement constructeur

---
+++ src/classes/Snake.cpp
@@ -0,0 +1,46 @@
+#include "Snake.h"
+#include "Point.h"
+#include <iostream>
+
+using namespace std;
+
+
+Snake::Snake()
+{
+	m_index=1;
+
+
+}
+
+Snake::~Snake()
+{
+
+
+
+}
+
+void Snake::ajouterPoint(Point p)
+{
+	/*int blockBoucle=0;//variable qui va bloquer la boucle si on ajoute un caractere dans le tableau serpent
+	do{
+	for (int i = 0; i < 20; ++i)
+	{
+		if (serpent[i]!='*')
+		{
+			this->serpent[m_index]=p;
+			blockBoucle=1;
+		}
+	}
+}while(blockBoucle==0);
+
+*/
+
+
+
+
+}
+
+
+
+
+
---
+++ src/classes/Snake.h
@@ -0,0 +1,27 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+
+#include "Point.h"
+#include <ncurses.h>
+
+
+class Point;
+class Snake
+{
+
+private:
+	Point serpent[20];
+	int m_index;
+
+
+public:
+	Snake();
+	~Snake();
+	void ajouterPoint(Point p);
+	void afficher();
+	
+	
+};
+
+#endif
